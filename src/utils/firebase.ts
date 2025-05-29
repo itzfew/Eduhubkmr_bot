18cc6475977f73d673f5bafdabc41ab7754c1244
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-29 19:13:35 +0530

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
- import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, push, set } from 'firebase/database';
+// src/utils/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, set, onValue, remove } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDNME0C__0yE84_fmabt9_qhnv7l4Y2osg',
@@ -15,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, push, set };
+export { db, ref, push, set, onValue, remove };
